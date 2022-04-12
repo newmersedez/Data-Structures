6894a442675110654b280ba2ae3a843614b74986
newmersedez <trishkk2001@gmail.com>
2022-04-13 02:13:20 +0300

    std::pair implemented

---
+++ pair.hpp
@@ -0,0 +1,63 @@
+#pragma once
+
+namespace ft
+{
+	template< class T1, class T2>
+	struct pair
+	{
+		typedef	T1	first_type;
+		typedef T2	second_type;
+	
+		T1	first;
+		T2	second;
+
+		pair(void)
+			: first(), second()
+		{}
+		
+		template<class U, class V>
+		pair (const pair<U, V>& pr)
+			: first(pr.first), second(pr.second)
+		{}
+
+		pair (const T1& a, const T2& b)
+			: first(a), second(b)
+		{}
+		
+		pair& operator=(const pair& pr)
+        {
+            if (this == &pr)
+                return (*this);
+            this->first = pr.first;
+            this->second = pr.second;
+            return *this;
+        }
+
+		~pair(void)
+		{}
+	};
+
+	template <class T1, class T2>
+	bool operator==(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs)
+	{ return lhs.first == rhs.first && lhs.second == rhs.second; }
+
+	template <class T1, class T2>
+	bool operator!=(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs)
+	{ return !(lhs == rhs); }
+
+	template <class T1, class T2>
+	bool operator<(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs)
+	{ return lhs.first < rhs.first || (!(rhs.first < lhs.first) && lhs.second < rhs.second); }
+
+	template <class T1, class T2>
+	bool operator<=(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs)
+	{ return !(rhs < lhs); }
+
+	template <class T1, class T2>
+	bool operator>(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs)
+	{ return rhs < lhs; }
+
+	template <class T1, class T2>
+	bool operator>=(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs)
+	{ return !(lhs < rhs); }
+}
